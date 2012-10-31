0b0076d4ae4bbc663331d30edcf318b1a3d8c229
David <ragerdl@gmail.com>
2012-10-30 22:26:12 -0500

    Saving memory model before cleaning it up, so that I have access to the lemmas that I needed at various times via git

--- acl2/memory.lisp
+++ acl2/memory.lisp
@@ -1,4 +1,12 @@
+; Rager would like to have a more records-based approach so that he can get the
+; double-write theorem that records provie.  However, it's non-trivial
+; (perhapss impossible) to prove that if update-loc is given a memory-p, that
+; it will return a memory-p.  This is a property that Rager wants, so he's
+; giving up on this file for now.
+
 (in-package "ACL2")
+
+(include-book "defexec/other-apps/records/records" :dir :system)
 
 (include-book "cutil/defalist" :dir :system)
 (include-book "misc/defun-plus" :dir :system)
@@ -7,12 +15,24 @@
   (declare (ignore x))
   t)
 
-(cutil::defalist memory-p (x)
-  :key (natp x)
-  :val (memory-val-p x)
-  :keyp-of-nil nil
-  :valp-of-nil t
-  :true-listp t)
+;; (cutil::defalist memory-p1 (x)
+;;   :key (natp x)
+;;   :val (memory-val-p x)
+;;   :keyp-of-nil nil
+;;   :valp-of-nil t
+;;   :true-listp t)
+
+(defn memory-p1 (lst)
+  (cond ((atom lst)
+         (null lst))
+        (t (and (consp (car lst))
+                (natp (caar lst))
+                (memory-val-p (cdar lst))
+                (memory-p1 (cdr lst))))))
+
+(defn memory-p (x)
+  (and (memory-p1 x)
+       (good-map x)))
 
 (defun+ empty-memory-p (mem)
 
@@ -28,40 +48,96 @@
                                (empty-memory-p (empty-memory)))))
   nil)
 
+(defun update-loc (i val mem)
+  (declare (xargs :guard (and (memory-p mem)
+                              (natp i)
+                              (memory-val-p val))))      
+  (mset i val mem))
+
+;; (defthm subgoal-1-1
+;;   (implies (good-map x)
+;;            (equal (map->acl2 x)
+;;                   x))
+;;   :hints (("Goal" :in-theory (enable map->acl2))))
+
+;; (defthm subgoal-1-1b
+;;   (implies (good-map x)
+;;            (equal (acl2->map x)
+;;                   x))
+;;   :hints (("Goal" :in-theory (enable acl2->map))))
+
+
+;; (defthm subgoal-1
+;;   (implies (and (integerp i) (<= 0 i))
+;;            (memory-p1 (mset i val nil)))
+;;   :hints (("Goal" :in-theory (enable mset))))
+
+;; (defthm subgoal-3
+;;   (implies (and (consp mem)
+;;               (consp (car mem))
+;;               (cdar mem)
+;;               (not (cdr mem))
+;;               (integerp (caar mem))
+;;               (<= 0 (caar mem))
+;;               (integerp i)
+;;               (<= 0 i))
+;;          (memory-p1 (mset i val mem)))
+;;   :hints (("Goal" :in-theory (enable mset))))
+
+
+;; (thm ; subgoal-5
+;;   (implies (and (consp mem)
+;;                 (consp (car mem))
+;;                 (good-map (cdr mem))
+;;                 (cdar mem)
+;;                 (cdr mem)
+;;                 (memory-p1 (mset i val (cdr mem)))
+;;                 (integerp (caar mem))
+;;                 (<= 0 (caar mem))
+;;                 (memory-p1 (cdr mem))
+;;                 (<< (caar mem) (caadr mem))
+;;                 (integerp i)
+;;                 (<= 0 i))
+;;            (memory-p1 (mset i val mem)))
+;;   :hints (("Goal" :in-theory (e/d (extend-records)
+;;                                   ((ill-formed-key)
+;;                                    ill-formed-key
+;;                                    )))))
+
+
+(in-theory (enable extend-records))
+
 (defun+ update-loc (i val mem)
   (declare (xargs :guard (and (memory-p mem)
                               (natp i)
                               (memory-val-p val))
                   :output (memory-p (update-loc i val mem))))
-  (acons i val mem))
-
-; I don't think having a lemma of empty_empty will help ACL2 solve any problems
-; it couldn't solve before, so I'm leaving it out.
+  (mset i val mem))
 
 (defun+ lookup-loc (i mem)
   (declare (xargs :guard (and (memory-p mem)
                               (natp i))
                   :output (memory-val-p (lookup-loc i mem))))
-  (let ((val (assoc-equal i mem)))
-    (cond (val (cdr val))
-          (t nil))))
+  (mget i mem))
+
+(in-theory (disable extend-records))
 
 (defthm read-of-write
-  (implies (and (memory-p mem)
-                (natp i))
-           (equal (lookup-loc i (update-loc i val mem))
-                  val)))
+  (equal (lookup-loc i (update-loc i val mem))
+         val))
+
 (defthm read-of-non-write
-  (implies (and (memory-p mem)
-                (natp i)
-                (natp j)
-                (not (equal i j)))
+  (implies (not (equal i j))
            (equal (lookup-loc i (update-loc j val mem))
                   (lookup-loc i mem))))
 
+(defthm write-of-write
+  (equal (update-loc i val (update-loc i val2 mem))
+         (update-loc i val mem)))
+
+
 (defthm read-of-empty-mem
-  (implies (and (memory-p mem)
-                (empty-memory-p mem))
+  (implies (empty-memory-p mem)
            (equal (lookup-loc i mem)
                   nil)))
 
